807877bb2ecc31c728a1d2facb2af695986d205f
Clint Armstrong <clint@clintarmstrong.net>
2020-05-13 01:01:07 -0400

    progress on entries

--- src/entry.rs
+++ src/entry.rs
@@ -5,6 +5,7 @@ use crate::ipnet::TryToMac;
 use std::convert::TryInto;
 use std::convert::{From, TryFrom};
 
+use crate::network::InterfaceNetwork;
 use globset::Glob;
 use ipnetwork::IpNetwork;
 use log::warn;
@@ -14,7 +15,6 @@ use std::io::{self, Write};
 use std::iter;
 use std::net::IpAddr;
 use tabwriter::TabWriter;
-use crate::network::InterfaceNetwork;
 
 pub struct Entry {
     pub name: String,
@@ -40,6 +40,32 @@ impl Entry {
     }
 }
 
+pub fn entries_from_val(val: Value) -> impl Iterator<Item = Entry> {
+    match val {
+        Value::Sequence(seq) => entries_from_seq(seq),
+        _ => entries_from_seq(vec![val]),
+    }
+}
+
+fn entries_from_seq(seq: serde_yaml::Sequence) -> impl Iterator<Item = Entry> {
+    seq.into_iter()
+        .filter_map(|v| match v {
+            Value::Mapping(map) => Some(entries_from_map(map)),
+            _ => {
+                warn!("invalid entry map: {:?}", v);
+                None
+            }
+        })
+        .flatten()
+}
+
+fn entries_from_map(map: Mapping) -> impl Iterator<Item = Entry> {
+    map.into_iter().filter_map(|(k, v)| {
+        let nets = InterfaceNetwork::filtered(&k);
+        None
+    })
+}
+
 pub trait AsEntries {
     fn as_entries(&self) -> Box<dyn Iterator<Item = Entry> + '_>;
 }
@@ -59,11 +85,10 @@ impl AsEntries for Value {
 
 impl AsEntries for Mapping {
     fn as_entries(&self) -> Box<dyn Iterator<Item = Entry> + '_> {
-        self.iter().map(|(k, v)|  {
+        self.iter().map(|(k, v)| {
             let net = InterfaceNetwork::filtered(k);
             ""
-        }
-    );
+        });
         todo!()
     }
 }
